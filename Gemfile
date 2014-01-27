b118a2082ba40357d6db2c642fc84d75d012a5f9
Scott Williams <scott@swilliams.me>
2014-01-27 13:38:29 -0700

    Added activesupport

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'webmock', '~> 1.16.0'
+gem 'activesupport', '~> 4.0.0'
 
 # Specify your gem's dependencies in arkenstone.gemspec
 gemspec
--- lib/arkenstone.rb
+++ lib/arkenstone.rb
@@ -2,6 +2,7 @@ require 'net/http'
 require 'arkenstone/version'
 require 'arkenstone/hook'
 require 'arkenstone/env'
+require 'arkenstone/associations'
 require 'arkenstone/document'
 require 'arkenstone/timestamps'
 
