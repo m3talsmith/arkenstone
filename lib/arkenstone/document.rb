1afd3b4c431e3f0915d4d8d1e2b8cb6c06dc9730
Scott Williams <scott@swilliams.me>
2014-02-14 11:32:19 -0700

    Documentation

--- lib/arkenstone/document.rb
+++ lib/arkenstone/document.rb
@@ -156,6 +156,7 @@ module Arkenstone
     module ClassMethods
       attr_accessor :arkenstone_url, :arkenstone_attributes, :arkenstone_content_type, :arkenstone_hooks, :arkenstone_inherit_hooks
 
+      ### Sets the root url used for generating RESTful requests.
       def url(new_url)
         self.arkenstone_url = new_url
       end
@@ -164,15 +165,60 @@ module Arkenstone
         "#{full_url(self.arkenstone_url)}query"
       end
 
+      # == Hooks
+      #
+      # Hooks are used to allow you to call arbitrary code at various points in the object lifecycle. For example, if you need to massage some property names before they are sent off to the `url`, you can do that with a hook. A hook should extend `Arkenstone::Hook` and then override the method you want to hook into. There are three types of hooks:
+      # 1. `before_request` - Called before the request is sent to the web service. Passes in the request environment (an `Arkenstone::Environment`) as a parameter.
+      # 2. `after_complete` - Called after the request has been *successfully* completed. Passes in a Net::HTTPResponse as a parameter.
+      # 3. `on_error` - Called if the response returned an error. Passes in a Net::HTTPResponse as a parameter.
+      #
+      # Example:
+      #
+      #     class ErrorLogger < Arkenstone::Hook
+      #       def on_error(response)
+      #         # log the error here
+      #       end
+      #     end
+      #
+      #     class User
+      #       include Arkenstone::Document
+      #
+      #       url 'http://example.com/users'
+      #       add_hook ErrorLogger.new
+      #     end
       def add_hook(hook)
         self.arkenstone_hooks = [] if self.arkenstone_hooks.nil?
         self.arkenstone_hooks << hook
       end
 
+      # Hooks are applied **only** to the class they are added to. This can cause a problem if you have a base class and want to use the same hooks for subclasses. If you want to use the same hooks as a parent class, use `inherit_hooks`. This will tell Arkenstone to walk up the inheritance chain and call all of the hooks it can find.
+      # Example:
+      #
+      #     class ErrorLogger < Arkenstone::Hook
+      #       def on_error(response)
+      #         # log the error here
+      #       end
+      #     end
+      #
+      #     class BaseModel
+      #       include Arkenstone::Document
+      #
+      #       add_hook ErrorLogger.new
+      #       add_hook SomeOtherHook.new
+      #     end
+      #
+      #     class User < BaseModel
+      #       url 'http://example.com/users'
+      #
+      #       inherit_hooks
+      #     end
+      #       
+      # This will use the hooks defined for `BaseModel` and any defined for `User` too.
       def inherit_hooks(val = true)
         self.arkenstone_inherit_hooks = val
       end
 
+      ### Sets the attributes for an Arkenstone Document. These become `attr_accessors` on instances.
       def attributes(*options)
         self.arkenstone_attributes = options
         class_eval("attr_accessor :#{options.join(', :')}")
@@ -183,6 +229,7 @@ module Arkenstone
         self.arkenstone_content_type = new_content_type
       end
 
+      ### Constructs a new instance with the provided attributes.
       def build(options)
         document = self.new
         document.attributes = options.select do |key, value|
@@ -191,6 +238,7 @@ module Arkenstone
         return document
       end
 
+      ### Builds a list of objects with attributes set from a JSON string.
       def parse_all(json)
         return [] if json.nil? or json.empty?
         tree = JSON.parse json
@@ -198,11 +246,13 @@ module Arkenstone
         Arkenstone::QueryList.new documents
       end
 
+      ### Creates and saves a single instance with the attribute values provided.
       def create(options)
         document = self.build(options)
         document.save
       end
 
+      ### Performs a GET request to the instance url with the supplied id. Builds an instance with the response.
       def find(id)
         url      = full_url(self.arkenstone_url) + id.to_s
         response = self.send_request url, :get
@@ -211,6 +261,7 @@ module Arkenstone
       end
 
       # TODO: all of the http/network stuff is getting pretty big, I'd like to refactor it all out to its own module.
+      # All http requests go through here. 
       def send_request(url, verb, data=nil)
         http = create_http url
         request_env = Arkenstone::Environment.new url: url, verb: verb, body: data
@@ -223,6 +274,7 @@ module Arkenstone
         response
       end
 
+      ### Takes appropriate action if the request was a success or failure.
       def handle_response(response)
         if response_is_success response
           call_response_hooks response
@@ -231,6 +283,7 @@ module Arkenstone
         end
       end
 
+      ### Creates the http object used for requests.
       def create_http(url)
         uri = URI(url)
         http = Net::HTTP.new(uri.hostname, uri.port)
@@ -238,11 +291,13 @@ module Arkenstone
         http
       end
 
+      ### Builds a Net::HTTP request object for the appropriate verb.
       def build_request(url, verb)
         klass = eval("Net::HTTP::#{verb.capitalize}")
         klass.new URI(url)
       end
 
+      ### Fills in the body of a request with the appropriate serialized data.
       def set_request_data(request, data)
         case self.arkenstone_content_type
         when :form
@@ -254,16 +309,21 @@ module Arkenstone
         end
       end
 
+      ### Sets HTTP headers on the request.
       def set_request_headers(request, headers)
         headers.each do |key, val|
           request.add_field key, val
         end
       end
 
+      ### Determines if the response was successful.
+      # TODO: Refactor this to handle more status codes.
+      # TODO: How do we handle redirects (30x)? 
       def response_is_success(response)
         %w(200 204).include? response.code
       end
 
+      ### Calls the `arkenstone_url` expecting to receive a json array of properties to deserialize into a list of objects.
       def all
         response        = self.send_request self.arkenstone_url, :get
         documents       = parse_all response.body
@@ -290,18 +350,22 @@ module Arkenstone
         end
       end
 
+      ### Calls all of the available `before_request` hooks available for the class.
       def call_request_hooks(request)
         call_hook Proc.new { |h| h.before_request request }
       end
 
+      ### Calls all of the available `after_complete` hooks available for the class.
       def call_response_hooks(response)
         call_hook Proc.new { |h| h.after_complete response }
       end
 
+      ### Calls all of the available `on_error` hooks available for the class.
       def call_error_hooks(response)
         call_hook Proc.new { |h| h.on_error response }
       end
 
+      ### Executes the appropriate `hook` and walks the inheritance tree if the `arkenstone_inherit_hooks` flag has been set.
       def call_hook(enumerator)
         hooks = []
         if self.arkenstone_inherit_hooks == true
